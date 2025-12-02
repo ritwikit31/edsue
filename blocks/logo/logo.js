7b4807121ce7234a5db0d0ea645518deb68f503f
gazal2708 <gazaljain7523@gmail.com>
2025-12-02 19:31:30 +0530

    changes in fstab

--- blocks/logo/logo.js
+++ blocks/logo/logo.js
@@ -3,14 +3,21 @@ import { getTextContent } from '../../scripts/utils/dom.js';
 const decorate = (block) => {
   if (!block) return;
 
-  const [iconCol, urlCol] = block.children ?? [];
-  if (!iconCol || !urlCol) return;
-
-  const anchor = urlCol.querySelector('a');
+  // Account for imageMimeType field from custom-asset
+  const [iconCol, mimeTypeCol, altTextCol, urlCol] = block.children ?? [];
+  
+  // If using custom-asset, iconCol has image, mimeTypeCol is mimetype, altTextCol is alt, urlCol is link
+  // If using regular reference, iconCol has image, urlCol is link (no mimetype)
+  
+  // Try to find the URL column (has <a> tag)
+  const urlColumn = [...block.children].find(col => col.querySelector('a'));
+  if (!urlColumn) return;
+  
+  const anchor = urlColumn.querySelector('a');
   const targetHref = anchor?.href ?? '#';
   const logoAltText = getTextContent(anchor) ?? '';
 
-  const imgEl = iconCol.querySelector('img');
+  const imgEl = iconCol?.querySelector('img');
   let imgMarkup = '';
 
   if (imgEl != null) {
